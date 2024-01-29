fcd4aae77bbf5fdb94687f2bf8ef1afed729c13c
Maicon Lucinda <maiconlucinda@gmail.com>
2024-01-29 08:22:26 +0000

    Day03_LastPart

--- Day03/theory/main.py
+++ Day03/theory/main.py
@@ -1,6 +1,7 @@
 #Day 3 - Beginner - Control Flow and Logical Operators
 
-# > Grater than
+# > Grater thanexit
+
 # < Less than
 # >= Grater or equal than
 # <= Less or equal than
@@ -57,6 +58,46 @@ if height >= 120:
     wants_photo = input("Do you want a photo taken? Y or N ")
     if wants_photo == "Y":
         bill += 3
+
+    print(f"Your final bill is ${bill}")
+
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+
+
+
+#Logical Operators
+# A and B = both need to be true in order to the result ro be true
+# C or D = One of the option needs to be true in order to the result be true.
+# not E = Invert the value of the result, if it is True, i becomes False.
+
+
+
+
+# Working with Operators
+bill = 0
+if height >= 120: 
+    print("You can ride the rollercoaster!")
+    age = int(input("How old are you? "))
+    if age < 12:
+        print("Child tickets are $5.")
+        bill = 5
+    elif age <= 18:
+        print("Youth tickets are $7.")
+        bill = 7
+
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a free ride on us!.")
+
+    else:
+        print("Adult tickets are $12.")
+        bill = 12
+    
+    wants_photo = input("Do you want a photo taken? Y or N ")
+    if wants_photo == "Y":
+        bill += 3
+
 
     print(f"Your final bill is ${bill}")
 
