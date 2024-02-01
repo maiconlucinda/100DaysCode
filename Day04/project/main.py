ae235c3aa26636dfd155714d614bcba5ec995b71
Maicon Lucinda <maiconlucinda@gmail.com>
2024-02-01 00:06:39 +0000

    Day03_Beginner

---
+++ Day04/project/main.py
@@ -0,0 +1,16 @@
+#Day 4 - Beginner - Randomisation and Python Lists
+
+# Modulo Random, creating randomness
+import random
+
+# Integer
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+# Float
+random_float = random.random()
+print(random_float)
+
+# Exemplo prático
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
